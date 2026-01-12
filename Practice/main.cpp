d389523ad850dd090df3ad3b4dbec5e2f0836c28
Thomas <trosselet@gaming.tech>
2026-01-12 19:28:17 +0100

    [DEV][ADD] C++ Exercice

---
+++ Practice/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+//Niveau 0 : Découverte de la console
+
+//Exercice 1 : Bonjour le monde
+
+//Objectif : afficher du texte à l’écran.
+//Exemple : afficher Hello, World!ou ton prénom.
+
+//Exercice 2 : Addition simple
+
+//Objectif : utiliser les variables et les entrées / sorties.
+//Demande à l’utilisateur de rentrer deux nombres et affiche leur somme.
+
+
+//Niveau 1 : Conditions
+
+//Exercice 3 : Pair ou impair
+
+//Objectif : apprendre les conditions(if / else) et l’opérateur modulo %).
+//Demande à l’utilisateur un nombre et indique s’il est pair ou impair.
+
+//Exercice 4 : Plus grand nombre
+
+//Objectif : utiliser plusieurs conditions.
+//Demande deux nombres et affiche lequel est le plus grand.
+
+
+//Niveau 2 : Boucles
+
+//Exercice 5 : Compter jusqu’à N
+
+//Objectif : apprendre les boucles(for ou while).
+//Demande un nombre N et affiche tous les nombres de 1 à N.
+
+//Exercice 6 : Somme des nombres
+
+//Objectif : combiner boucle et addition.
+//Demande un nombre N et calcule la somme de tous les nombres de 1 à N.
+
+
+//Niveau 3 : Tableaux simples
+
+//Exercice 7 : Stocker des nombres
+
+//Objectif : apprendre à utiliser un tableau.
+//Demande à l’utilisateur 5 nombres et affiche-les.
+
+
+
+int main() {
+    std::cout << "Hello, Practice project!" << std::endl;
+    return 0;
+}
